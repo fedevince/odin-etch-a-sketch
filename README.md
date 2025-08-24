7cae83dcf9059b266c4fefd8921a961ed3758a39
Federico Vincelli <vincellifederico@gmail.com>
2025-08-24 19:12:52 +0200

    Implement first version of createGrid function

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # odin-etch-a-sketch
+
+Etch-a-Sketch site
---
+++ script.js
@@ -0,0 +1,26 @@
+//TODO
+// Create 16 x 16 div grid, set flexbox to wrap them
+let size = 16;
+const container = document.querySelector(".container");
+function createGrid(size)
+    // delete existing grid
+    // not square? 
+    {for (let i = 0; i < size * size; i++) {
+        let box = document.createElement("div");
+        box.classList.add("box");
+        box.style.width = `${100/size}%`;
+        box.style.height = box.width;
+        container.appendChild(box);
+    }}
+
+createGrid(size);
+// create container
+// append 16 x 16 divs to it
+
+// Set up hover effect
+
+// Button on the top of the screen asking for the number of squares in the grid
+// Remove existing grid; new one in the same space
+
+// EXTRA: randomize squares RGB values
+// EXTRA: progressive darkening
